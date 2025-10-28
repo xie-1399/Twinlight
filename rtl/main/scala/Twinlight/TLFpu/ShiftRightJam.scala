673010396902e5512537c60488ee122f639a2017
Yuchen Wang <kevin56348@gmail.com>
2025-10-28 21:07:58 +0800

    feat: add ShiftRightJam
    add ShiftRightJam & the corresponding unit test
    
    ShiftRightJam shifts a number by x bits, and return the sticky bits for further float operations

---
+++ rtl/main/scala/Twinlight/TLFpu/ShiftRightJam.scala
@@ -0,0 +1,33 @@
+package Twinlight.TLFpu
+
+import Twinlight.TLUtils.TLPlugin.TLModule
+import spinal.core._
+import spinal.lib._
+
+import scala.language.postfixOps
+
+class ShiftRightJam(len: Int, expWidth: Int) extends TLModule {
+  val max_shamt = log2Up(len + 1)
+  val io = new Bundle {
+    val i = in port Bits(len bits)
+    val shamt = in port UInt(expWidth bits)
+    val o = out port Bits(len bits)
+    val stiky = out port Bool()
+  }
+
+  val exceed = io.shamt > len
+  val shamt = io.shamt.resize(max_shamt bits)
+  val stickyMask = ((U(1) << shamt) - 1).resize(len bits).asBits | B(exceed) #* len
+
+  io.o := Mux(exceed, B(0, len bits), (io.i >> shamt).resize(len bits))
+  io.stiky := (io.i.asBits & stickyMask).asBools.reduceBalancedTree({ case (x, y) => x | y })
+}
+
+object ShiftRightJam {
+  def apply(i: Bits, shamt: UInt): (Bits, Bool) = {
+    val shifter = new ShiftRightJam(i.getWidth, shamt.getWidth)
+    shifter.io.i := i
+    shifter.io.shamt := shamt
+    (shifter.io.o, shifter.io.stiky)
+  }
+}
---
+++ rtl/test/scala/Twinlight/TLFpu/ShiftRightJamTest.scala
@@ -0,0 +1,62 @@
+package Twinlight.TLFpu
+
+import Twinlight.TLSim._
+import org.scalatest.funsuite.AnyFunSuite
+import spinal.core.sim._
+
+import scala.language.postfixOps
+import scala.util.Random
+
+class ShiftRightJamTest extends AnyFunSuite {
+
+  test(" ShiftRightJam random test") {
+    SIMCFG(gtkFirst = true).compile {
+      val dut = new ShiftRightJam(32, 8)
+      dut
+    }.doSimUntilVoid {
+      dut =>
+        dut.clockDomain.forkStimulus(10)
+        SimTimeout(1000000 * 10)
+
+        def monitor() = {
+          val testThread = fork {
+            val gen = new Random()
+            val err = Array.tabulate(32767)({ i =>
+              dut.io.i #= gen.nextInt(0x7fffffff)
+              var lb = 0
+              var ub = 32
+              if (i < 10000) {
+                lb = 0
+                ub = 32
+              } else if (i < 15000) {
+                lb = 32
+                ub = 64
+              } else {
+                lb = 0
+                ub = (1 << 8) - 1
+              }
+              dut.io.shamt #= gen.nextInt(ub - lb) + lb
+              dut.clockDomain.waitSampling()
+              val o = dut.io.o.toInt
+              val s = dut.io.stiky.toBoolean
+
+              val std_o = if (dut.io.shamt.toInt >= 32) 0 else dut.io.i.toInt >>> dut.io.shamt.toInt
+              val std_sticky = Array.tabulate(32)({ i =>
+                if (i < dut.io.shamt.toInt) dut.io.i.toBooleans(i) else false
+              }).reduce(_ | _)
+              (std_o == o) && (std_sticky == s)
+            }).map { i => if (i) 0 else 1 }.sum
+
+            if (err == 0) {
+              simSuccess()
+            } else {
+              simFailure(s"$err errors found!!!")
+            }
+          }
+        }
+
+        monitor()
+    }
+  }
+
+}
