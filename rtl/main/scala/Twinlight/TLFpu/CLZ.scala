6783eb0b3da0b7f04394fe3531f6dcd6b9dcd23f
Yuchen Wang <kevin56348@gmail.com>
2025-11-06 16:44:17 +0800

    feat: add CLZ module
    add CLZ module, without test.

---
+++ rtl/main/scala/Twinlight/TLFpu/CLZ.scala
@@ -0,0 +1,27 @@
+package Twinlight.TLFpu
+
+import Twinlight.TLUtils.TLPlugin.TLModule
+import spinal.core._
+import spinal.lib._
+
+import scala.language.postfixOps
+
+class CLZ(len: Int) extends TLModule{
+  val inWidth = len
+  val outWidth = U(inWidth - 1).getWidth
+
+  val io = new Bundle{
+    val clz_in = in port UInt(inWidth bits)
+    val clz_out = out port UInt(outWidth bits)
+  }
+
+  io.clz_out := io.clz_in.asBits.reversed.asBools.sFindFirst({i => i===True})
+}
+
+object CLZ {
+  def apply(value: UInt): UInt = {
+    val clz = new CLZ(value.getWidth)
+    clz.io.clz_in := value
+    clz.io.clz_out
+  }
+}
