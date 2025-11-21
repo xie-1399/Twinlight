32a5f42b2c3ca268f914d2aa3d6291239465a692
Yuchen Wang <kevin56348@gmail.com>
2025-11-22 00:57:48 +0800

    test: add inf/NaN test

--- rtl/test/scala/Twinlight/TLFpu/Fadd32Test.scala
+++ rtl/test/scala/Twinlight/TLFpu/Fadd32Test.scala
@@ -9,6 +9,9 @@ import scala.language.postfixOps
 import scala.util.Random
 
 case class BasicFloatTools() {
+  // subnormal: s 00000000 f
+  // inf      : s 11111111 ==0
+  // NaN      : s 11111111 !=0
   def FP2Int(x: Float): Int = {
     // only 32 bits version now
     java.lang.Float.floatToRawIntBits(x)
@@ -16,6 +19,49 @@ case class BasicFloatTools() {
 
   def Int2FP(x: Int): Float = {
     java.lang.Float.intBitsToFloat(x)
+  }
+
+  def isNan(x: Float): Boolean = {
+    val xint = FP2Int(x)
+    isNan(xint)
+  }
+
+  def isNan(x: Int): Boolean = {
+    val xint = x
+    val exp = (xint & 0x7F800000) >> 23
+    val mantissa = xint & 0x007FFFFF
+    exp == 0xFF && mantissa != 0
+  }
+
+  def genRand(): (BigInt, Float) = {
+    val gen = new Random()
+    val randi = gen.nextInt(100)
+
+    val rand_percent = 70
+    val inf_percent = 10
+    val nan_percent = 10
+    val subnormal_percent = 10
+
+    assert(rand_percent + inf_percent + nan_percent + subnormal_percent == 100)
+
+    val rand_threshold = 0 + rand_percent
+    val inf_threshold = rand_threshold + inf_percent
+    val nan_threshold = inf_threshold + nan_percent
+    val subnormal_threshold = nan_threshold + subnormal_percent
+
+    assert(subnormal_threshold == 100)
+
+    val xint = (if (randi < rand_threshold) { // normal
+      BigInt((if (gen.nextBoolean()) -1 else 1) * gen.nextInt(Int.MaxValue))
+    } else if (randi < inf_threshold) { // Inf
+      BigInt(if (gen.nextBoolean()) "7F800000" else "FF800000", 16)
+    } else if (randi < nan_threshold) { // NaN
+      BigInt(if (gen.nextBoolean()) "7F800000" else "FF800000", 16) + gen.nextInt((1 << 23) - 1) + 1
+    } else { // subnormal
+      BigInt(if (gen.nextBoolean()) "00000000" else "80000000", 16) + gen.nextInt((1 << 23) - 1) + 1
+    }).mod(BigInt(1) << 32)
+    val xfloat = Int2FP(xint.toInt)
+    (xint, xfloat)
   }
 }
 
@@ -32,22 +78,25 @@ class Fadd32Test extends AnyFunSuite {
 
         def monitor() = {
           val testThread = fork {
-            val testCase = 1 << 20
-            val gen = new Random()
+            val testCase = 1 << 10
             val tool = BasicFloatTools()
             val err = Array.tabulate(testCase)({ i =>
-              val a = (gen.nextFloat() - 0.0f) * (1024000111 >> ((i + 0) % 32))
-              val b = (gen.nextFloat() - 0.0f) * (1024000111 >> ((i + 7) % 32))
-              dut.io.a #= BigInt(tool.FP2Int(a))
-              dut.io.b #= BigInt(tool.FP2Int(b))
+              val (a, fa) = tool.genRand()
+              val (b, fb) = tool.genRand()
+              dut.io.a #= a
+              dut.io.b #= b
               dut.io.rm #= 0
               dut.clockDomain.waitSampling(1)
               // check
-              val res = dut.io.result.toBigInt
-              val std_res = tool.FP2Int(a + b)
-              //              println(s"a + b = $a + $b = ${tool.Int2FP(std_res)}")
-              //              println(s"res   = ${tool.Int2FP(res.toInt)}")
-              res == std_res
+              val res = dut.io.result.toInt
+              val std_res = tool.FP2Int(fa + fb)
+              if ((res != std_res) && !(tool.isNan(res) && tool.isNan(std_res))) {
+                println(s"a + b = $fa + $fb = $a + $b = ${tool.Int2FP(std_res)} = $std_res")
+                println(s"res   = $fa + $fb = $a + $b = ${tool.Int2FP(res)} = $res")
+                println(" ")
+              }
+
+              (res == std_res) || (tool.isNan(res) && tool.isNan(std_res))
             }).map({ x => if (x) 0 else 1 }).sum
 
             if (err == 0) {
