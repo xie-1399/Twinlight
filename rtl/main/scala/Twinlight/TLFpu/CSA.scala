c681a0a641f8323b3027f8b541339792fd115ff8
Yuchen Wang <kevin56348@gmail.com>
2025-12-24 19:10:31 +0800

    refactor: improve the clarity of the CSA coding style.

--- rtl/main/scala/Twinlight/TLFpu/CSA.scala
+++ rtl/main/scala/Twinlight/TLFpu/CSA.scala
@@ -15,38 +15,34 @@ abstract class CarrySaveAdderMToN(m: Int, n: Int)(len: Int) extends TLModule {
 
 // TODO: coding style optimization required.
 class CSA2_2(len: Int) extends CarrySaveAdderMToN(2, 2)(len) {
-  val temp = Vec.fill(len)(UInt(2 bits))
-  for ((t, i) <- temp.zipWithIndex) {
-    val (a, b) = (io.inx(0)(i), io.inx(1)(i))
-    val sum = a ^ b
-    val cout = a & b
-    t := cout.asUInt @@ sum.asUInt
-  }
-  io.outx.zipWithIndex.foreach({ case (x, i) =>
-    x := Cat(temp.reverse.map(_(i))).asUInt
-  })
+  val (a, b) = (io.inx(0), io.inx(1))
+  val sum = a ^ b
+  val cout = a & b
+
+  io.outx(0) := sum
+  io.outx(1) := cout
 }
 
 class CSA3_2(len: Int) extends CarrySaveAdderMToN(3, 2)(len) {
-  val temp = Vec.fill(len)(UInt(2 bits))
-  for ((t, i) <- temp.zipWithIndex) {
-    val (a, b, cin) = (io.inx(0)(i), io.inx(1)(i), io.inx(2)(i))
-    val a_xor_b = a ^ b
-    val a_and_b = a & b
-    val sum = a_xor_b ^ cin
-    val cout = a_and_b | (a_xor_b & cin)
-    t := cout.asUInt @@ sum.asUInt
-  }
-  io.outx.zipWithIndex.foreach({ case (x, i) =>
-    x := Cat(temp.reverse.map(_(i))).asUInt
-  })
+  val (a, b, cin) = (io.inx(0), io.inx(1), io.inx(2))
+  val a_xor_b = a ^ b
+  val a_and_b = a & b
+  val sum = a_xor_b ^ cin
+  val cout = a_and_b | (a_xor_b & cin)
+
+  io.outx(0) := sum
+  io.outx(1) := cout
 }
 
 class CSA5_3(len: Int) extends CarrySaveAdderMToN(5, 3)(len) {
   val FAs = Array.fill(2)(new CSA3_2(len))
+  //                                  in0, in1, in2
   FAs(0).io.inx := Vec.tabulate(3)({i => io.inx(i)})
+  //                         sum          in3          cin
   FAs(1).io.inx := Vec(FAs(0).io.outx(0), io.inx(3), io.inx(4))
-  io.outx := Vec(FAs(1).io.outx(0), FAs(0).io.outx(1), FAs(1).io.outx(1))
+  io.outx(0) := FAs(1).io.outx(0) // sum
+  io.outx(1) := FAs(0).io.outx(1) // <<1
+  io.outx(2) := FAs(1).io.outx(1) // <<2
 }
 
 case class C22() extends CSA2_2(1)
