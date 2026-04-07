0d97285e25cebff0dd7d34744823193712986393
Yuchen Wang <kevin56348@gmail.com>
2026-04-07 20:53:51 +0800

    feat: add pipeline registers to BitSliceMultiplier & test

--- rtl/main/scala/Twinlight/TLFpu/BitSliceMultiplier.scala
+++ rtl/main/scala/Twinlight/TLFpu/BitSliceMultiplier.scala
@@ -7,7 +7,7 @@ import spinal.lib._
 
 import scala.language.postfixOps
 
-case class BitSliceMultiplier(multiplicandWidth: Int, multiplierWidth: Int, sliceWidth: Int, wallaceTree: Boolean = true) extends TLModule {
+case class BitSliceMultiplier(multiplicandWidth: Int, multiplierWidth: Int, sliceWidth: Int, wallaceTree: Boolean = true, pipeAt: Seq[Int] = Seq()) extends TLModule {
   val io = new Bundle {
     val multiplicand = in port SInt(multiplicandWidth bits)
     val multiplier = in port SInt(multiplierWidth bits)
@@ -20,7 +20,7 @@ case class BitSliceMultiplier(multiplicandWidth: Int, multiplierWidth: Int, slic
 
   val partial_products = Vec.tabulate(multiplicandWidth / sliceWidth + 1) { i =>
     val pp_temp = multiplicand_slices(i) * io.multiplier
-    if (wallaceTree){
+    if (wallaceTree) {
       val s = pp_temp.msb
       val pp = if (i == 0) Cat(~s, s, s, pp_temp) else Cat(U"1", ~s, pp_temp)
       for (j <- 2 * i until 2 * i + pp.getWidth) {
@@ -34,11 +34,15 @@ case class BitSliceMultiplier(multiplicandWidth: Int, multiplierWidth: Int, slic
 
   if (wallaceTree) {
     // wallace tree version
-    val (sum, carry) = Multiplier(multiplicandWidth, Seq()).addAll(cols = columns, depth = 0)
+    val (sum, carry) = Multiplier(multiplicandWidth, pipeAt).addAll(cols = columns, depth = 0)
     io.product := (sum + carry).asSInt
   } else {
     // reduce tree version
-    io.product := partial_products.reduceBalancedTree(_ + _)
+    // add latency manually
+    val prod_r = Vec.fill(pipeAt.size)(Reg(SInt(multiplicandWidth + multiplierWidth bits)))
+    prod_r(0) := partial_products.reduceBalancedTree(_ + _)
+    (1 until prod_r.size).foreach { x => prod_r(x) := prod_r(x - 1) }
+    io.product := prod_r.last
   }
 
 
--- rtl/test/scala/Twinlight/TLFpu/BitSliceMultiplierTest.scala
+++ rtl/test/scala/Twinlight/TLFpu/BitSliceMultiplierTest.scala
@@ -11,43 +11,73 @@ import scala.util.Random
 
 class BitSliceMultiplierTest extends AnyFunSuite {
 
-  test(" BitSliceMultiplierTest random test") {
-    SIMCFG().compile {
-      val dut = BitSliceMultiplier(multiplicandWidth = 8, multiplierWidth = 8, sliceWidth = 2, wallaceTree = false)
-      dut.multiplicand_slices.simPublic()
-      dut.partial_products.simPublic()
-      dut
-    }.doSimUntilVoid {
-      dut =>
-        dut.clockDomain.forkStimulus(10)
+  val tool = BasicFloatTools(fp32 = false, fp16 = true)
+  val pipelineStage = Seq(1)
+  val mulLatency = pipelineStage.size
 
-        def monitor() = {
-          val testThread = fork {
-            val gen = new Random()
-            val err = Array.tabulate(1 << 20)({ i =>
-              dut.io.multiplier.randomize()
-              dut.io.multiplicand.randomize()
+  Seq(true, false).foreach { wallaceTree =>
+    test(s"BitSliceMultiplierTest (wallaceTree ${if (wallaceTree) "en" else "dis"}able) random test") {
+      SIMCFG().compile {
+        val dut = BitSliceMultiplier(multiplicandWidth = 8, multiplierWidth = 8, sliceWidth = 2, wallaceTree = wallaceTree, pipeAt = pipelineStage)
+        dut.multiplicand_slices.simPublic()
+        dut.partial_products.simPublic()
+        dut
+      }.doSimUntilVoid {
+        dut =>
+          dut.clockDomain.forkStimulus(10)
 
-              dut.clockDomain.waitSampling()
-              val o = dut.io.product.toInt
-              val std_o = dut.io.multiplier.toInt * dut.io.multiplicand.toInt
-              if (o != std_o){
-                println(s"${dut.io.multiplier.toInt} * ${dut.io.multiplicand.toInt} = ${std_o}")
-                println(s"ours: ${o}")
+          def monitor() = {
+            val testThread = fork {
+              val ans_queue = mutable.Queue[(Int, Int, Int)]()
+              val latency = mulLatency
+              val testCase = 1 << 20
+              var err = 0
+
+              val load_thread = fork {
+                for (_ <- 0 until testCase) {
+                  val a = dut.io.multiplier.randomize()
+                  val b = dut.io.multiplicand.randomize()
+                  val std_res = (a * b).toInt
+                  ans_queue.enqueue((a.toInt, b.toInt, std_res))
+                  dut.clockDomain.waitSampling(1)
+                }
               }
-              o == std_o
-            }).map { i => if (i) 0 else 1 }.sum
+              dut.clockDomain.waitSampling(latency)
+              val check_thread = fork {
+                for (_ <- 0 until testCase) {
+                  dut.clockDomain.waitSampling(1)
+                  val res = dut.io.product.toInt
+                  val std = ans_queue.dequeue()
+                  val std_res = std._3
+                  val a = std._1
+                  val b = std._2
+                  val iseq = res == std_res
+                  if (!iseq) {
+                    println(s"${a} * ${b} = ${std_res}")
+                    println(s"ours: ${res}")
+                    err += 1
+                  }
+                }
+              }
 
-            if (err == 0) {
-              simSuccess()
-            } else {
-              simFailure(s"$err errors found!!!")
+              load_thread.join()
+              check_thread.join()
+
+              dut.clockDomain.waitSampling(100)
+
+              if (err == 0) {
+                println(s"=====Test Passed!!!=====")
+                simSuccess()
+              } else {
+                simFailure(s"=====Failed!!! $err Occurred!!!=====")
+              }
             }
           }
-        }
 
-        monitor()
+          monitor()
+      }
     }
+
   }
 
 }
